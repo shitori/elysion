d2a4fa6da166810a39d3aa39bb15148bba0c16fc
Antony Fourchault <antonyfourchault@gmail.com>
2020-08-06 16:14:48 +0200

    cree 2 session une pour la partie recherche et une pour la partie elle meme

--- models/model.js
+++ models/model.js
@@ -10,22 +10,41 @@ class Model {
     }
 
     static create_game(players, name, cb) {
-        var r = Math.floor(Math.random() * 1000000) + 1;
-        connection.query("insert into game values (?, ? , default )", [r, name], (err) => {
+        connection.query("select * from game where name = ?", [name], (err, row) => {
             if (err) throw err
-            var sql = "insert into player values "
-            for (var i = 0; i < players.length; i++) {
-                if (i == players.length-1) {
-                    sql += "(default,?,default," + r + ");"
-                } else {
-                    sql += "(default,?,default," + r + "),"
-                }
-                console.log(sql)
+            if (row.length > 0) {
+                cb("ko")
+            } else {
+                var r = Math.floor(Math.random() * 1000000) + 1;
+                connection.query("insert into game values (?, ? , default )", [r, name], (err) => {
+                    if (err) throw err
+                    var sql = "insert into player values "
+                    for (var i = 0; i < players.length; i++) {
+                        if (i == players.length - 1) {
+                            sql += "(default,?,default," + r + ");"
+                        } else {
+                            sql += "(default,?,default," + r + "),"
+                        }
+                        console.log(sql)
+                    }
+                    connection.query(sql, players, (err) => {
+                        if (err) throw err
+                        cb("ok")
+                    })
+                })
             }
-            connection.query(sql, players, (err) => {
-                if (err) throw err
-                cb("ok")
-            })
+        })
+
+    }
+
+    static join_game(name, cb) {
+        console.log(name)
+        connection.query("select * from game where name = ? ", [name], (err, row) => {
+            if (row.length > 0) {
+                cb(true)
+            } else {
+                cb(false)
+            }
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,34 @@ router.get('/', function (req, res, next) {
     })
 });
 
+router.get('/game', function (req, res, next) {
+    models.test(function (date) {
+        console.log(date);
+        res.render('game', {title: 'Rejoindre une partie'});
+    })
+});
+
+
+router.post('/game', function (req, res, next) {
+    req.session.name = req.body.nom;
+    models.join_game(req.body.nom, function (exist) {
+        console.log(exist)
+        if (exist) {
+            res.render('playGame', {title: 'Rejoindre une partie', name: req.session.name});
+        } else {
+            console.log("la partie n'existe pas")
+            res.redirect("/game")
+        }
+    })
+});
+
+
+
+
 router.get('/creategame', function (req, res, next) {
     models.test(function (date) {
         console.log(date);
-        res.render('createGame', {title: 'Nouvelle partie'});
-
+        res.render('createGame', {title: 'Nouvelle partie', error: ""});
     })
 
 });
@@ -36,7 +59,16 @@ router.post('/creategame', function (req, res, next) {
     }
     models.create_game(joueur, nom, function (status) {
         console.log(status)
-        res.redirect("/")
+        if (status == "ko") {
+            res.render('createGame', {
+                title: 'Nouvelle partie',
+                error: "La partie n'a pas pu être crée. Veuillez essayer un autre nom."
+            });
+        } else {
+            req.session.name = nom
+            res.redirect("/game")
+        }
+
     })
 
 
