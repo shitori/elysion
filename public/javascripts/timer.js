bfe6f22edb4852dd46ab5e13b57313610fd39169
Antony Fourchault <antonyfourchault@gmail.com>
2020-09-06 00:25:57 +0200

    correctif timer + mode de jeu

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,26 +1,32 @@
-var timer = 90
+var timer = 10
 var s = timer;
 var rater = 3;
 var id_game = $("#id_game").val()
 var j1 = $("#j1").val()
 var j2 = $("#j2").val()
+var mot = $("#mot").val()
 
 $("#timer").text(s);
 $("#essai").text(rater);
 
-$(function () {
-    function timer() {
-        s--;
-        $("#timer").text(s);
-    }
+$("#word").click(function (){
+    $("#word").text(mot);
+    $(function () {
+        function timer() {
+            s--;
+            $("#timer").text(s);
+        }
 
-    setInterval(timer, 1000);
-});
-setTimeout(function () {
-    $.post("/game/" + id_game);
+        setInterval(timer, 1000);
+    });
+    setTimeout(function () {
+        $.post("/game/" + id_game);
 
-    window.location.reload(1);
-}, timer * 1000);
+        window.location.reload(1);
+    }, timer * 1000);
+})
+
+
 
 $("#rater").click(function () {
     rater--;
